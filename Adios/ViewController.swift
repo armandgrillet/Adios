67d38347242d5c1f281e530d255201fbc9ff0b7d
Armand Grillet <armand.grillet@outlook.com>
2015-07-06 22:17:18 +0200

    CloudKit is here

--- Adios/ViewController.swift
+++ Adios/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.reloadBlockerList(UIButton())
         // Do any additional setup after loading the view, typically from a nib.
     }
 
