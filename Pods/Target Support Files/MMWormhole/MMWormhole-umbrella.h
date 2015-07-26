3418be628a4656bf5d9c5cac393f7579e1f6b80d
Armand Grillet <armand.grillet@outlook.com>
2015-07-26 16:39:46 +0200

    Revert 2f72519..b47d295
    This rolls back to commit 2f725190cf45cb5e4c3a48f8343e05903fef8311.

--- Pods/Target Support Files/MMWormhole/MMWormhole-umbrella.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "MMWormhole.h"
-#import "MMWormholeCoordinatedFileTransiting.h"
-#import "MMWormholeFileTransiting.h"
-
-FOUNDATION_EXPORT double MMWormholeVersionNumber;
-FOUNDATION_EXPORT const unsigned char MMWormholeVersionString[];
-
