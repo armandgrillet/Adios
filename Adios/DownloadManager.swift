9fa414ca2d076e1307f8e93d29b20809f9cd84a2
Armand Grillet <armand.grillet@outlook.com>
2015-08-19 17:44:32 +0200

    CloudKit synchronisation finished
    Now it’s time to test.

--- Adios/DownloadManager.swift
+++ Adios/DownloadManager.swift
@@ -38,6 +38,20 @@ class DownloadManager {
                     self.listsManager.createList(list, records: rules)
                     if nextLists != nil && nextLists!.count > 0 { // Other lists need to be downloaded
                        self.downloadRulesFromList(nextLists![0], nextLists: Array(nextLists!.dropFirst()))
+                    } else { // Everything has been downloaded
+                        let pr = NSPredicate(format: "recordID = %@", CKRecordID(recordName: "TheOneAndOnly"))
+                        let queryGetUpdate = CKQuery(recordType: "Updates", predicate: pr)
+                        self.publicDB.performQuery(queryGetUpdate, inZoneWithID: nil) { results, error in
+                            if error != nil {
+                                print(error)
+                            } else {
+                                if let theOneAndOnlyUpdate = results?.first, let userDefaults = NSUserDefaults(suiteName: "group.AG.Adios") {
+                                    let currentUpdate = theOneAndOnlyUpdate["Version"]! as! Int
+                                    userDefaults.setInteger(currentUpdate, forKey: "currentUpdate")
+                                    userDefaults.synchronize()
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -47,51 +61,67 @@ class DownloadManager {
     }
     
     func getNewRecords(update: Int) {
-        var recordsCreated: [CKRecord] = []
-        var recordsDeleted: [CKRecord] = []
-        
-            // Get all the records that have been created after the last update
-            let predicate = NSPredicate(format: "Update > %@", update)
-            let queryCreatedRules = CKQuery(recordType: "Rules", predicate: predicate)
-            let queryCreatedRulesOperation = CKQueryOperation(query: queryCreatedRules)
-            queryCreatedRulesOperation.qualityOfService = .Utility // The user is waiting for the task to compelte
-            queryCreatedRulesOperation.recordFetchedBlock = { (rule: CKRecord) in
-                recordsCreated.append(rule)
+        if let userDefaults = NSUserDefaults(suiteName: "group.AG.Adios") {
+            var currentUpdate = userDefaults.integerForKey("currentUpdate")
+            if currentUpdate < 0 {
+                currentUpdate = 0
             }
-            queryCreatedRulesOperation.queryCompletionBlock = { (cursor : CKQueryCursor?, error : NSError?) in
-                if error != nil {
-                    print(error)
-                } else if cursor != nil { // The cursor is not nil thus we still have some records to download
-                    let newOperation = queryCreatedRulesOperation
-                    newOperation.cursor = cursor!
-                    self.publicDB.addOperation(newOperation)
-                } else { // We have added the rules, now we remove the old ones.
-                    let queryDeletedRules = CKQuery(recordType: "DeletedRules", predicate: predicate)
-                    let queryDeletedRulesOperation = CKQueryOperation(query: queryDeletedRules)
-                    queryDeletedRulesOperation.qualityOfService = .Utility // The user is waiting for the task to compelte
-                    queryDeletedRulesOperation.recordFetchedBlock = { (rule: CKRecord) in
-                        recordsDeleted.append(rule)
-                    }
-                    queryDeletedRulesOperation.queryCompletionBlock = { (cursor : CKQueryCursor?, error : NSError?) in
-                        if error != nil {
-                            print(error)
-                        } else if cursor != nil { // The cursor is not nil thus we still have some records to download
-                            let newOperation = queryDeletedRulesOperation
-                            newOperation.cursor = cursor!
-                            self.publicDB.addOperation(newOperation)
-                        } else {
-                            // We've updated the list, now we can call the content blocker to update it.
-                            print("done")
-                            //            userDefaults.setInteger(update, forKey: "\(list)Update")
-                            //            userDefaults.synchronize()
-                            self.listsManager.updateRulesWithRecords(recordsCreated, recordsDeleted: recordsDeleted)
-                        }
-                    }
-                    self.publicDB.addOperation(queryDeletedRulesOperation)
+            
+            if let followedLists = userDefaults.arrayForKey("followedLists") as! [String]? {
+                var referenceToFollowedLists: [CKReference] = []
+                for list in followedLists {
+                    referenceToFollowedLists.append(CKReference(recordID: CKRecordID(recordName: list), action: .DeleteSelf))
                 }
+                
+                var recordsCreated: [CKRecord] = []
+                var recordsDeleted: [CKRecord] = []
+                
+                // Get all the records that have been created after the last update
+                let currentUpdate = 0
+                let predicate = NSPredicate(format: "(Update > \(currentUpdate)) AND (List IN %@)", referenceToFollowedLists)
+                let queryCreatedRules = CKQuery(recordType: "Rules", predicate: predicate)
+                let queryCreatedRulesOperation = CKQueryOperation(query: queryCreatedRules)
+                queryCreatedRulesOperation.qualityOfService = .Utility // The user is waiting for the task to complete
+                queryCreatedRulesOperation.recordFetchedBlock = { (rule: CKRecord) in
+                    recordsCreated.append(rule)
+                }
+                queryCreatedRulesOperation.queryCompletionBlock = { (cursor : CKQueryCursor?, error : NSError?) in
+                    if error != nil {
+                        print(error)
+                    } else if cursor != nil { // The cursor is not nil thus we still have some records to download
+                        let newOperation = queryCreatedRulesOperation
+                        newOperation.cursor = cursor!
+                        self.publicDB.addOperation(newOperation)
+                    } else { // We have added the rules, now we remove the old ones.
+                        let queryDeletedRules = CKQuery(recordType: "DeletedRules", predicate: predicate)
+                        let queryDeletedRulesOperation = CKQueryOperation(query: queryDeletedRules)
+                        queryDeletedRulesOperation.qualityOfService = .Utility // The user is waiting for the task to compelte
+                        queryDeletedRulesOperation.recordFetchedBlock = { (rule: CKRecord) in
+                            recordsDeleted.append(rule)
+                        }
+                        queryDeletedRulesOperation.queryCompletionBlock = { (cursor : CKQueryCursor?, error : NSError?) in
+                            if error != nil {
+                                print(error)
+                            } else if cursor != nil { // The cursor is not nil thus we still have some records to download
+                                let newOperation = queryDeletedRulesOperation
+                                newOperation.cursor = cursor!
+                                self.publicDB.addOperation(newOperation)
+                            } else {
+                                // We've updated the list, now we can call the content blocker to update it.
+                                print("done")
+                                print(recordsCreated)
+                                print(recordsDeleted)
+                                userDefaults.setInteger(update, forKey: "currentUpdate")
+                                userDefaults.synchronize()
+                                self.listsManager.updateRulesWithRecords(recordsCreated, recordsDeleted: recordsDeleted)
+                            }
+                        }
+                        self.publicDB.addOperation(queryDeletedRulesOperation)
+                    }
+                }
+                publicDB.addOperation(queryCreatedRulesOperation)
             }
-        
-            publicDB.addOperation(queryCreatedRulesOperation)
+        }
     }
     
     func downloadLists(lists: [String]) {
